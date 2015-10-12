192a57c132b7428e82a6d1d5f8c6e347d73280c8
pikachuturkey <forumyok@gmail.com>
2015-10-12 17:57:07 +0300

    Update common.php
    bazı satırlarda "hakkımızda" yerine "hakkımızda sayfası" ifadesi eklendi.

--- language/tr/common.php
+++ language/tr/common.php
@@ -19,16 +19,16 @@ if (empty($lang) || !is_array($lang))
 
 $lang = array_merge($lang, array(
 	'ABOUTUS'						=> 'Hakkımızda',
-	'ABOUTUS_SETTINGS'				=> 'Hakkımızda ayarları',
-	'ABOUTUS_UPDATED'				=> 'Hakkımızda başarıyla güncellendi.',
+	'ABOUTUS_SETTINGS'				=> 'Hakkımızda sayfası ayarları',
+	'ABOUTUS_UPDATED'				=> 'Hakkımızda sayfası başarıyla güncellendi.',
 	'ABOUTUS_AND'					=> 've',
 	
 	'ACP_ABOUTUS_ENABLE'			=> 'Hakkımızda sayfasını aktive et',
 	'ACP_ABOUTUS_ENABLE_EXPLAIN'	=> 'Eğer "Hakkımızda" sayfası kapatılırsa "Hakkımızda" sayfası linki sitenizin altında gösterilmeyecek. Ne var ki "app.php/aboutus" URLsi ile "Hakkımızda" sayfasına ulaşabilirsiniz. ',
-	'ACP_ABOUTUS_INFO'				=> 'Hakkımızda metni',
+	'ACP_ABOUTUS_INFO'				=> 'Hakkımızda sayfası metni',
 	'ACP_ABOUTUS_INFO_EXPLAIN'		=> 'Hakkımızda sayfasında gösterilecek metni buradan düzenleyebilirsiniz.',
-	'ACP_ABOUTUS_INFO_PREVIEW'		=> 'Hakkımızda önizleme',
-	'ACP_ABOUTUS_SETTINGS'			=> 'Hakkımızda ayarları',
+	'ACP_ABOUTUS_INFO_PREVIEW'		=> 'Hakkımızda sayfası önizleme',
+	'ACP_ABOUTUS_SETTINGS'			=> 'Hakkımızda sayfası ayarları',
 	'ACP_ABOUTUS_SETTINGS_EXPLAIN'	=> 'Buradan "Hakkımızda" sayfasının metnini belirleyebilir ve "Hakkımızda"-sayfasını aktive ve deaktive edebilirsiniz.',
 
 	'ACP_TERMS_OF_USE_ENABLE' 			=> 'Kullanım şartlarını göster',
